cf7581ce28fcdbf13bf7bc797e63431b1cd77803
ariannagreco3393 <a.greco3393@gmail.com>
2022-04-21 15:55:05 +0200

    milestone 2 add dinamic markup

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,35 +26,35 @@ Bonus:
 const app = new Vue({
     el: '#app',
     data: {
-        slider: {
-            nations: [
-                {
-                    image: './assets/img/01.jpg',
-                    title: 'Svezia',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                },
-                {
-                    image: './assets/img/02.jpg',
-                    title: 'Svizzera',
-                    text: 'Lorem ipsum.',
-                },
-                {
-                    image: './assets/img/03.jpg',
-                    title: 'Gran Bretagna',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                },
-                {
-                    image: './assets/img/04.jpg',
-                    title: 'Germania',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-                },
-                {
-                    image: './assets/img/05.jpg',
-                    title: 'Paradise',
-                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-                }
-            ]
-        }
+        activeImage: 0,
+        nations: [
+            {
+                image: './assets/img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: './assets/img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: './assets/img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: './assets/img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: './assets/img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ]
+
     },
     methods: {
 
