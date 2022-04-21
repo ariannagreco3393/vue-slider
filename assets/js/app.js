3ad6d480180878ab3ec8d58163106c9c7f2e4c33
ariannagreco3393 <a.greco3393@gmail.com>
2022-04-21 15:14:55 +0200

    add milestone 1 static markup

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,29 +27,29 @@ const app = new Vue({
     el: '#app',
     data: {
         slider: {
-            images: [
+            nations: [
                 {
-                    image: 'assets/img/01.jpg',
+                    image: './assets/img/01.jpg',
                     title: 'Svezia',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
                 },
                 {
-                    image: 'assets/img/02.jpg',
+                    image: './assets/img/02.jpg',
                     title: 'Svizzera',
                     text: 'Lorem ipsum.',
                 },
                 {
-                    image: 'assets/img/03.jpg',
+                    image: './assets/img/03.jpg',
                     title: 'Gran Bretagna',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
                 },
                 {
-                    image: 'assets/img/04.jpg',
+                    image: './assets/img/04.jpg',
                     title: 'Germania',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
                 },
                 {
-                    image: 'assets/img/05.jpg',
+                    image: './assets/img/05.jpg',
                     title: 'Paradise',
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
                 }
